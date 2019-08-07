3ac1cdc7463489273a33f38443c83b114a863058
RWahl8177 <rwahl005@west-mec.org>
2019-08-07 15:28:30 -0700

    Continued work
    Continued work on the page.

--- sub-unit-01-DocumentObjectModel/zoom.js
+++ sub-unit-01-DocumentObjectModel/zoom.js
@@ -2,8 +2,8 @@
 
  *    Photo zoom
  *    Variables and functions
- *    Author: 
- *    Date:   
+ *    Author: Ryan Wahl
+ *    Date:   08.07.19
 
  *    Filename: zoom.js
  */
@@ -17,6 +17,20 @@ var figFilename = "images/IMG_0" + photoOrderArray[2] + ".jpg";
 /* populate img element and create event listener */
 function pageSetup() {
    document.getElementsByTagName("img")[0].src = figFilename; // assign filename to img element
+}
+
+function closeWin(){
+   window.close;
+}
+
+function createEventListener(){
+   var closeWindowDiv = document.getElementsByTagName("p")[0];
+   if(closeWindowDiv.addEventListener){
+      closeWindowDiv.addEventListener("click", closeWin, false);
+   }
+   else if(closeWindowDiv.attachEvent){
+      closeWindowDiv.attachEvent("onclick", closeWin);
+   }
 }
 
 /* add img src value and create event listener when page finishes loading */
